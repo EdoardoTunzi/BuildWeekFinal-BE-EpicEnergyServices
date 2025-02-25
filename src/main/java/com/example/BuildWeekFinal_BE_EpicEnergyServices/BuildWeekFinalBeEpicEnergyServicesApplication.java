d7bbf68a35d05517f3d707daf119aaf9c3b2e5ea
Edoardo <edfl.tunzi@gmail.com>
2025-02-25 11:37:12 +0100

    initial commit

---
+++ src/main/java/com/example/BuildWeekFinal_BE_EpicEnergyServices/BuildWeekFinalBeEpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.BuildWeekFinal_BE_EpicEnergyServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeekFinalBeEpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeekFinalBeEpicEnergyServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BuildWeekFinal_BE_EpicEnergyServices/BuildWeekFinalBeEpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuildWeekFinal_BE_EpicEnergyServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeekFinalBeEpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
